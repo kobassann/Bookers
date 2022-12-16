0ac07fe380443bd616d1f45dfb7c0c49667b18f6
kobassann <108574108+kobassann@users.noreply.github.com>
2022-12-16 14:54:06 +0000

    [add]review-bookers destroy-function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def create
     @book = Book.new(book_params)
     @book.save
@@ -21,9 +21,10 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
-    book = book.destroy
+    @book.destroy
+    redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
