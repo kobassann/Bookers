bef37ba43062620462a8d8bc33155e8d01f9cc9a
kobassann <108574108+kobassann@users.noreply.github.com>
2022-12-26 15:14:30 +0000

    [add]review-bookers flash-message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if@book.save
+    flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
